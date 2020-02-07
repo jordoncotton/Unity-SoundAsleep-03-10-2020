06793bfb79d647d11853cacb65709a6bc56e79b9
Jordon C <42358762+jordoncotton@users.noreply.github.com>
2020-02-07 15:06:48 -0600

    push

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+ //   public float minimumX = -60f;
+ //   public float maximumX = 60f;
+ //   public float minimumY = -360f;
+ //   public float maximumY = -360f;
+
+ //   public float sensitivityX = 15f;
+ //   public float sensitivityY = 15f;
+
+ //   public Camera cam;
+
+ //   float rotationX = 0f;
+ //   float rotationY = 0f;
+
+	//// Use this for initialization
+	//void Start ()
+ //   {
+ //       Cursor.lockstate = CursorLockMode.Locked;
+
+ //       if(Input.GetKey(Keycode.Escape))
+ //       {
+ //           Cursor.lockstate = CursorLockMode.None;
+ //           Cursor.visible
+ //       }
+	//}
+	
+	//// Update is called once per frame
+	//void Update ()
+ //   {
+ //       rotationY += Input.GetAxis("Mouse X") * sensitivityY;
+ //       rotationX += Input.GetAxis("Mouse Y") * sensitivityX;
+ //       rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+
+ //       transform.localEulerAngles = new Vector3(0, rotationY, 0);
+ //       cam.transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+	//}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed;
+
+    CharacterController character;
+    public Vector3 moveDirection;
+
+    void Awake()
+    {
+        character = GetComponent<CharacterController>();   
+
+    }
+
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        character.SimpleMove((new Vector3(horizontalMovement, 0,verticalMovement ).normalized) * walkSpeed);
+    }
+
+    //void FixedUpdate()
+    //{
+    //    Move();   
+    //}
+
+    //void Move()
+    //{
+    //    //Vector3 yVelFix = new Vector3(0, rb.velocity.y, 0);
+    //    character.velocity = moveDirection * walkSpeed * Time.deltaTime;
+    //    //rb.velocity += yVelFix;
+    //}
+}
