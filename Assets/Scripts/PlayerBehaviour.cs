be26f97deba266ee46a81b099c560f8842d5f8bd
Juan Manzanares <micahmanzanares@gmail.com>
2020-02-18 17:20:06 -0600

    r

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+   
+
+    public int EndValue;
+
+    // Use this for initialization
+    void Start ()
+    {
+        EndValue = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Guffy"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                EndValue++;
+                other.gameObject.SetActive(false);
+
+            }
+        }
+    }
+}
