f8f532dc999f81a81395b9127d6dd4d5815b73c3
Juan Manzanares <micahmanzanares@gmail.com>
2020-03-10 14:51:30 -0500

    Getting Ready to move to new build

--- Assets/Scripts/MonsterBehaviour.cs
+++ Assets/Scripts/MonsterBehaviour.cs
@@ -16,6 +16,15 @@ public class MonsterBehaviour : MonoBehaviour
 
     public Vector3 Ref;
 
+    public bool IdleState; //state of monster when idle
+    public bool RoamingState; //state of monster when roamoming its normal path
+    public bool AlertedState; //state of monster when alerted to noise
+    public bool InvestingState; //state of monster when moving towards the noise
+    public bool SearchState; //state of monster when searching for the source of the noise at the position that the noise was made
+    public bool DiscoveryState; //state of monster when it discovers the source of the noise and belives the source of the noise is something that needs to be destroyed
+    public bool ChaseState; //state of monster  when it runs towards the source of the noise
+    public bool KillState; //state of monster  when it kills the player
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +38,17 @@ public class MonsterBehaviour : MonoBehaviour
 
         Ref = new Vector3(0.0f, 0.0f, 0.0f);
 
-    }
+
+        IdleState = false;
+        RoamingState = true;
+        AlertedState = false;
+        InvestingState = false;
+        SearchState = false;
+        DiscoveryState = false;
+        ChaseState = false;
+        KillState = false;
+
+}
 
     void NextPoint()
     {
